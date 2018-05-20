be096d9e70d9923a6938a5690c748b4758a438d2
Jacob McGill <jacob@networktocode.com>
2018-05-19 21:55:22 -0400

    Update test_index_order.py

--- tests/test_index_order.py
+++ tests/test_index_order.py
@@ -64,7 +64,7 @@ def test_index_ordering():
         'a10', 'alcatel_sros', 'arista_eos', 'aruba_os', 'avaya_ers', 'avaya_vsp',
         'brocade_fastiron', 'brocade_netiron', 'brocade_nos', 'brocade_vdx', 'brocade_vyos',
         'checkpoint_gaia', 'cisco_asa', 'cisco_ios', 'cisco_nxos', 'cisco_s300', 'cisco_wlc',
-        'cisco_xe', 'cisco_xr','dell_force10', 'enterasys', 'extreme', 'f5_ltm', 'fortinet',
+        'cisco_xe', 'cisco_xr', 'dell_force10', 'enterasys', 'extreme', 'f5_ltm', 'fortinet',
         'hp_comware', 'hp_procurve', 'huawei', 'juniper', 'juniper_junos', 'linux', 'ovs_linux',
         'paloalto_panos', 'quanta_mesh', 'vmware_nsxv', 'vyatta_vyos', 'vyos'
         ] 
