6e54baec6091d5327cd29150cae6ddbeaa95a1ee
Olof Lundgren <olof@teknograd.no>
2018-10-01 20:31:44 +0200

    ubiquiti edgeswitch show arp, show vlan

--- tests/test_index_order.py
+++ tests/test_index_order.py
@@ -66,8 +66,8 @@ def test_index_ordering():
         'checkpoint_gaia', 'cisco_asa', 'cisco_ios', 'cisco_nxos', 'cisco_s300', 'cisco_wlc',
         'cisco_xe', 'cisco_xr', 'dell_force10', 'enterasys', 'extreme', 'f5_ltm', 'fortinet',
         'hp_comware', 'hp_procurve', 'huawei', 'juniper', 'juniper_junos', 'juniper_screenos',
-        'alcatel_sros', 'linux', 'ovs_linux', 'paloalto_panos', 'quanta_mesh', 'vmware_nsxv',
-        'vyatta_vyos', 'vyos'
+        'alcatel_sros', 'linux', 'ovs_linux', 'paloalto_panos', 'quanta_mesh',
+        'ubiquiti_edgeswitch', 'vmware_nsxv', 'vyatta_vyos', 'vyos'
     ]
 
     prior_os = ""
