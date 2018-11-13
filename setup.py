045da6a094416091fe6c52f8bccd18a1ee7d80a5
Micah Culpepper <micahculpepper@gmail.com>
2018-11-13 11:24:35 -0600

    simplify version number gathering

--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
 """setup.py file."""
-import re
+from ntc_templates import __version__
 from codecs import open
 from setuptools import setup
-
-version = ''
-with open('ntc_templates/__init__.py', 'r') as fd:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
-
-if not version:
-    raise RuntimeError('Cannot find version information')
 
 with open('README.md', 'r', 'utf-8') as f:
     readme = f.read()
@@ -23,7 +15,7 @@ long_description = readme + '\n\n' + history
 config = {
     'name': 'ntc_templates',
     'packages': ['ntc_templates'],
-    'version': version,
+    'version': __version__,
     'package_data': {'ntc_templates': ['templates/*']},
     'description': 'Package to return structured data from the output of network devices.',
     'long_description': long_description,
