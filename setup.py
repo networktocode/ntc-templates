7a0133121ebd2abf89d531e95e867328f9f78b18
Micah Culpepper <micahculpepper@gmail.com>
2018-11-19 08:24:52 -0600

    revert version-identification change. See discussion on https://github.com/networktocode/ntc-templates/pull/288

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-from ntc_templates import __version__
+import re
 from codecs import open
 from setuptools import setup
+
+version = ''
+with open('ntc_templates/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
 
 with open('README.md', 'r', 'utf-8') as f:
     readme = f.read()
@@ -13,7 +21,7 @@ long_description = readme + '\n\n' + history
 config = {
     'name': 'ntc_templates',
     'packages': ['ntc_templates'],
-    'version': __version__,
+    'version': version,
     'package_data': {'ntc_templates': ['templates/*']},
     'description': 'Package to return structured data from the output of network devices.',
     'long_description': long_description,
