eda5fcdc3844475730f539c4ccc0b51b6058a6c6
jmcgill298 <jacob@networktocode.com>
2019-04-13 23:08:22 -0400

    TEST CASES EXIST: Improve efficiency
    * Change ``KNOWN_MISSING_TESTS`` from a list to a set
    * Only perform glob when current entry is not in ``KNOWN_MISSING_TESTS``
    * Move ``assert`` to actual test case and still provide custom message

--- tests/test_testcases_exists.py
+++ tests/test_testcases_exists.py
@@ -3,29 +3,26 @@ import glob
 
 from tests import load_index_data
 
-KNOWN_MISSING_TESTS = [
+KNOWN_MISSING_TESTS = {
     'cisco_ios_show_vlan',
     'cisco_nxos_show_interface_brief',
     'cisco_nxos_show_ip_ospf_neighbor_vrf',
     'cisco_xr_show_controllers',
     '.*vyos.*_os_show_interfaces',
     '.*vyos.*_os_show_arp',
-]
+}
 
 
 def test_verify_parsed_and_reference_data_exists():
     """Verify that at least one test exists for all entries in the index file."""
     index = sorted(load_index_data())
-    coverage = {}
     for row in index:
         template = row[0].strip()
         platform = row[2].strip()
         cut = len(platform) + 1
         command = template[cut:].split('.')[0]
-        cases = 'tests/%s/%s/*.raw' % (platform, command)
-        test_list = glob.glob(cases)
-        coverage['%s_%s' % (platform, command)] = len(test_list)
-
-    for test in coverage:
-        if coverage[test] == 0 and test not in KNOWN_MISSING_TESTS:
-            assert test == 'No test cases found'
+        platform_command = '%s_%s' % (platform, command)
+        if platform_command not in KNOWN_MISSING_TESTS:
+            cases = 'tests/%s/%s/*.raw' % (platform, command)
+            test_list = glob.glob(cases)
+            assert len(test_list) != 0, 'Could not find tests for %s' % template
