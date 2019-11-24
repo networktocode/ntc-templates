55504b414e88ef56e5c1f2d8e7ed65c794e877a8
Mikhail Yohman <mikhail.yohman@gmail.com>
2019-11-24 22:15:35 +0000

    Added doc strings

--- tests/test_testcases_exists.py
+++ tests/test_testcases_exists.py
@@ -7,8 +7,10 @@ import pytest
 from tests import load_index_data
 
 
-def return_index():
-    """Used to parametrize and report each test case properly"""
+def extract_index_data():
+    """Used to parametrize and report each test case
+    with the necessary data
+    """
     index = sorted(load_index_data())
     refined_index = []
     for row in index:
@@ -25,7 +27,7 @@ def return_index():
     return refined_index
 
 
-@pytest.mark.parametrize("platform_directory,command,template", return_index())
+@pytest.mark.parametrize("platform_directory,command,template", extract_index_data())
 def test_verify_parsed_and_reference_data_exists(platform_directory, command, template):
     """Verify that at least one test exists for all entries in the index file.
     """
