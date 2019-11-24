3f0d793de98ed5966dac67cd6a3bb5b608bc311f
Mikhail Yohman <mikhail.yohman@gmail.com>
2019-11-24 21:19:16 +0000

    Fixed missing tests

--- tests/test_testcases_exists.py
+++ tests/test_testcases_exists.py
@@ -2,27 +2,15 @@
 import os
 import glob
 import re
+import pytest
 
 from tests import load_index_data
 
 
-KNOWN_MISSING_TESTS = {
-    "cisco_ios_show_vlan",
-    "cisco_nxos_show_interface_brief",
-    "cisco_nxos_show_ip_ospf_neighbor_vrf",
-    "cisco_xr_show_controllers",
-}
-
-
-def test_verify_parsed_and_reference_data_exists():
-    """Verify that at least one test exists for all entries in the index file.
-
-    TODO:
-        Add test cases for ``KNOWN_MISSING_TESTS`` and remove related conditional.
-        Remove "_ssh" from ``cisco_wlc_ssh`` and rely on vendor_platform_command syntax
-            instead of using regex on the directories.
-    """
+def return_index():
+    """Used to parametrize and report each test case properly"""
     index = sorted(load_index_data())
+    refined_index = []
     for row in index:
         template = row[0].strip()
         template_short = template.split(".template")[0]
@@ -33,7 +21,14 @@ def test_verify_parsed_and_reference_data_exists():
                 break
         cut = len(platform_directory) + 1
         command = template_short[cut:]
-        if template_short not in KNOWN_MISSING_TESTS:
-            cases = "tests/{0}/{1}/*.raw".format(platform_directory, command)
-            test_list = glob.glob(cases)
-            assert len(test_list) != 0, "Could not find tests for {0}".format(template)
+        refined_index.append((platform_directory, command, template))
+    return refined_index
+
+
+@pytest.mark.parametrize("platform_directory,command,template", return_index())
+def test_verify_parsed_and_reference_data_exists(platform_directory, command, template):
+    """Verify that at least one test exists for all entries in the index file.
+    """
+    cases = "tests/{0}/{1}/*.raw".format(platform_directory, command)
+    test_list = glob.glob(cases)
+    assert len(test_list) != 0, "Could not find tests for {0}".format(template)
