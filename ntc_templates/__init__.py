9dfe5c31ec190cbada4820b8042a1ff288478966
itdependsnetworks <ken@celenza.org>
2022-10-31 23:33:02 -0400

    Change docs to mkdocs, update dev environment, move to click for scripts

--- ntc_templates/__init__.py
+++ ntc_templates/__init__.py
@@ -1,3 +1,9 @@
 """ntc_templates - Parse raw output from network devices and return structured data."""
 
-__version__ = "3.0.0"
+try:
+    from importlib import metadata
+except ImportError:
+    # Python version < 3.8
+    import importlib_metadata as metadata
+
+__version__ = metadata.version(__name__)
