6048d13147bf415f6f24cc7c9d802d22f604c917
Josh VanDeraa <jv@networktocode.com>
2023-11-05 20:40:48 -0600

    Updates to help tests.

--- tasks.py
+++ tasks.py
@@ -29,7 +29,7 @@ def strtobool(val: str) -> bool:
         return False
 
     # Raise error if not valid truth value
-    raise ValueError("invalid truth value %r" % (val,))
+    raise ValueError(f"Invalid truth value {val}")
 
 
 def is_truthy(arg):
@@ -52,7 +52,7 @@ PYPROJECT_CONFIG = toml.load("pyproject.toml")
 TOOL_CONFIG = PYPROJECT_CONFIG["tool"]["poetry"]
 
 # Can be set to a separate Python version to be used for launching or building image
-PYTHON_VER = os.getenv("PYTHON_VER", "3.7")
+PYTHON_VER = os.getenv("PYTHON_VER", "3.9")
 # Name of the docker image/image
 IMAGE_NAME = os.getenv("IMAGE_NAME", TOOL_CONFIG["name"])
 # Tag for the image
